9c736aff79d60151f9f87fdf0ba5e17509510256
Vasyl Stetsuryn <vasyl@vasyl.org>
2025-06-17 17:17:49 +0200

    Add version as a build arg

--- main.go
+++ main.go
@@ -5,7 +5,14 @@ import (
 	server_http "github.com/st-vasyl/echo-server/pkg/v1/server_http"
 )
 
+var (
+	// Service info
+	Branch     = ""
+	Version    = ""
+	CommitHash = ""
+)
+
 func main() {
 	go server_grpc.Run()
-	server_http.Run()
+	server_http.Run(Branch, CommitHash, Version)
 }
